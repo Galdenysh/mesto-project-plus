ac8cc4e491f1c6d3e806b8ca07bd25fb817d55f0
Igor Petrov <galdenysh@air-igor.jugru.org>
2022-11-14 22:03:25 +0300

    feat: add user contriller

---
+++ src/controllers/users.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import user from "../models/user";
+
+export const getUsers = async (req: Request, res: Response) => {
+  try {
+    const users = await user.find({});
+    return res.status(200).send(users);
+  } catch (err) {
+    return res.status(500).send({ message: "Error while processing request" });
+  }
+};
+
+export const getUser = async (req: Request, res: Response) => {
+  try {
+    const currentUser = await user.findById(req.params._id);
+    return res.status(200).send(currentUser);
+  } catch (err) {
+    return res.status(500).send({ message: "Error while processing request" });
+  }
+};
+
+export const createUser = async (req: Request, res: Response) => {
+  const { name, about, avatar } = req.body;
+  try {
+    const newUser = await user.create({ name, about, avatar });
+    return res.status(200).send(newUser);
+  } catch (err) {
+    return res.status(500).send({ message: "Error while processing request" });
+  }
+};
